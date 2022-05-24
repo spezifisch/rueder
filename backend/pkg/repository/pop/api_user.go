c7907455acbd7aad404f5ed66b652f184c70f18b
Pascal <spezifisch@users.noreply.github.com>
2022-05-24 07:10:27 +0200

    add more tests for api repo, new test structure using TestMain

---
+++ backend/pkg/repository/pop/api_user.go
@@ -0,0 +1,237 @@
+package pop
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/apex/log"
+	mapset "github.com/deckarep/golang-set"
+	"github.com/gofrs/uuid"
+	"github.com/spezifisch/rueder3/backend/pkg/api/controller"
+	"github.com/spezifisch/rueder3/backend/pkg/helpers"
+	"github.com/spezifisch/rueder3/backend/pkg/repository/pop/models"
+)
+
+// Folders returns folders
+func (r *APIPopRepository) Folders(claims *helpers.AuthClaims) (ret []controller.Folder, err error) {
+	if r == nil || r.pop == nil {
+		err = errors.New("invalid repository")
+		return
+	}
+	// check login
+	if claims == nil || !claims.IsValid() {
+		err = errors.New("invalid claims")
+		return
+	}
+
+	user := models.User{}
+	err = r.pop.Find(&user, claims.ID)
+	if err != nil {
+		err = errors.New("user doesn't exist")
+		return
+	}
+
+	folders := user.Folders
+
+	// update folders struct if we change feed titles
+	updateFolders := false
+
+	ret = make([]controller.Folder, len(folders))
+	for i := 0; i < len(folders); i++ {
+		ret[i].ID = folders[i].ID
+		ret[i].Title = folders[i].Title
+		ret[i].Feeds = make([]controller.Feed, len(folders[i].Feeds))
+
+		for feedIdx, feed := range folders[i].Feeds {
+			// take feed info from folder struct
+			folderFeed := &ret[i].Feeds[feedIdx]
+			folderFeed.ID = feed.ID
+			folderFeed.Title = feed.Title
+			folderFeed.Icon = feed.Icon
+
+			// get updated article count and other info
+			var currentFeed controller.Feed
+			currentFeed, err = r.GetFeed(feed.ID)
+			if err != nil {
+				return
+			}
+
+			if folderFeed.Title == "" && currentFeed.Title != "" {
+				log.WithField("feed_id", folderFeed.ID).Infof("old feed title '%s', new title '%s'", folderFeed.Title, currentFeed.Title)
+
+				folderFeed.Title = currentFeed.Title
+				updateFolders = true
+			}
+
+			folderFeed.Icon = currentFeed.Icon
+			folderFeed.URL = currentFeed.URL
+			folderFeed.SiteURL = currentFeed.SiteURL
+			folderFeed.ArticleCount = currentFeed.ArticleCount
+		}
+	}
+
+	if updateFolders {
+		log.Info("updating folders with new titles")
+		user.Folders = folders
+		if updErr := r.pop.UpdateColumns(&user, "folders"); updErr != nil {
+			log.WithError(updErr).Error("updating folders failed")
+		}
+	}
+
+	return
+}
+
+// ChangeFolders saves the folder structure for a user
+func (r *APIPopRepository) ChangeFolders(claims *helpers.AuthClaims, folders []controller.Folder) (err error) {
+	if r == nil || r.pop == nil {
+		err = errors.New("invalid repository")
+		return
+	}
+	// check login
+	if claims == nil || !claims.IsValid() {
+		err = errors.New("invalid claims")
+		return
+	}
+	// check params
+	if folders == nil {
+		err = errors.New("invalid folders")
+		return
+	}
+
+	// get user folder list
+	user := models.User{}
+	err = r.pop.Find(&user, claims.ID)
+	if err != nil {
+		err = errors.New("user doesn't exist")
+		return
+	}
+	if len(folders) > r.folderCountLimit {
+		err = errors.New("too many folders")
+		return
+	}
+
+	// build a hashmap so we can ensure feeds are only subscribed to once.
+	subscribedFeeds := mapset.NewSet(uuid.UUID{})
+	// for comparisons against empty uuids
+	nullUUID := uuid.NullUUID{}
+	// the sanitized folders struct for the DB
+	foldersJSON := make(models.Folders, len(folders))
+	// validate folder changes and copy them
+	for folderIdx, folder := range folders {
+		if len(folder.Feeds) > r.folderFeedCountLimit {
+			err = errors.New("too many feeds in folder")
+			return
+		}
+
+		folderID := folder.ID
+		if helpers.IsSameUUID(nullUUID.UUID, folderID) {
+			// new folder, generate UUID
+			folderID, err = uuid.NewV4()
+			if err != nil {
+				return
+			}
+		}
+
+		foldersJSON[folderIdx].ID = folderID
+		foldersJSON[folderIdx].Title = folder.Title
+		foldersJSON[folderIdx].Feeds = make([]models.LightFeed, len(folder.Feeds))
+
+		for feedIdx, feed := range folder.Feeds {
+			if helpers.IsSameUUID(nullUUID.UUID, feed.ID) {
+				err = errors.New("null uuid in feed")
+				return
+			}
+			if subscribedFeeds.Contains(feed.ID) {
+				err = errors.New("subscribed to a feed multiple times")
+				return
+			}
+			foldersJSON[folderIdx].Feeds[feedIdx].ID = feed.ID
+
+			feedInfo := models.Feed{}
+			err := r.pop.Find(&feedInfo, feed.ID)
+			if err != nil {
+				return fmt.Errorf("feed doesn't exist: %s", feed.ID)
+			}
+			// this feed is good
+
+			if feed.Title != "" {
+				// allow custom title
+				foldersJSON[folderIdx].Feeds[feedIdx].Title = feed.Title
+			} else {
+				// use default title
+				foldersJSON[folderIdx].Feeds[feedIdx].Title = feedInfo.Title.String
+			}
+			foldersJSON[folderIdx].Feeds[feedIdx].Icon = feedInfo.Icon.String
+
+			subscribedFeeds.Add(feed.ID)
+		}
+	}
+
+	// update folders in db
+	user.Folders = foldersJSON
+	err = r.pop.UpdateColumns(&user, "folders")
+	if err != nil {
+		return
+	}
+
+	// update list of subscribed feeds to detect unsubscribed feeds (in the feed worker)
+	err = r.updateUserFeeds(&user, &subscribedFeeds)
+	return
+}
+
+func (r APIPopRepository) updateUserFeeds(user *models.User, feedIDs *mapset.Set) (err error) {
+	if user == nil || feedIDs == nil {
+		return errors.New("nil user or feedIDs")
+	}
+
+	// it turns out that update of many-to-many relationships isn't yet implemented in pop. :(
+	// see: https://github.com/gobuffalo/pop/issues/136
+	// therefore we do it ourself...
+	userFeeds := []models.UserFeed{}
+	err = r.pop.Where("user_id = ?", user.ID).All(&userFeeds)
+	if err != nil {
+		return
+	}
+
+	// put current feed ids into a set
+	existingUserFeeds := mapset.NewSet(uuid.UUID{})
+	for _, uf := range userFeeds {
+		existingUserFeeds.Add(uf.FeedID)
+	}
+
+	// which feed entries need to be deleted?
+	deleteUserFeeds := existingUserFeeds.Difference(*feedIDs)
+	if deleteUserFeeds.Cardinality() > 0 {
+		log.WithField("delete", deleteUserFeeds).WithField("count", deleteUserFeeds.Cardinality()).Info("deleting user_feeds")
+		err = r.pop.RawQuery("DELETE FROM user_feeds WHERE user_id = ? AND feed_id in (?)", user.ID.String(), deleteUserFeeds.ToSlice()).Exec()
+		if err != nil {
+			return
+		}
+	}
+
+	// which feed entries need to be added?
+	addUserFeeds := (*feedIDs).Difference(existingUserFeeds)
+	if addUserFeeds.Cardinality() > 0 {
+		log.WithField("add", addUserFeeds).WithField("count", addUserFeeds.Cardinality()).Info("adding user_feeds")
+		err = r.pop.Create(toUserFeeds(user.ID, &addUserFeeds))
+		if err != nil {
+			return
+		}
+	}
+
+	return
+}
+
+// convert a user id with a set of feed ids to a slice of UserFeeds
+func toUserFeeds(userID uuid.UUID, uf *mapset.Set) []models.UserFeed {
+	if uf == nil {
+		return nil
+	}
+
+	ret := make([]models.UserFeed, (*uf).Cardinality())
+	for i, feedID := range (*uf).ToSlice() {
+		ret[i].UserID = userID
+		ret[i].FeedID = feedID.(uuid.UUID)
+	}
+	return ret
+}
---
+++ backend/pkg/repository/pop/api_user_test.go
@@ -0,0 +1,299 @@
+package pop
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/cockroachdb/copyist"
+	"github.com/gobuffalo/pop/v6"
+	"github.com/spezifisch/rueder3/backend/pkg/api/controller"
+	"github.com/spezifisch/rueder3/backend/pkg/helpers"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAPIUser_First(t *testing.T) {
+	defer copyist.Open(t).Close()
+	firstRunTest(t)
+}
+
+func TestAPIPopRepository_Folders(t *testing.T) {
+	defer copyist.Open(t).Close()
+	beforeEachTest(t)
+
+	testPop, err := pop.Connect("test")
+	assert.NoError(t, err)
+
+	// good claims
+	mockClaims := helpers.AuthClaims{
+		ID:         testUserID,
+		Origin:     "somewhere",
+		Name:       "someone",
+		OriginName: "somewhere:someone",
+	}
+
+	// some bad claims
+	invalidClaims1 := mockClaims
+	invalidClaims1.Name = ""
+
+	invalidClaims2 := mockClaims
+	invalidClaims2.Origin = ""
+	invalidClaims2.Name = ""
+	invalidClaims2.OriginName = ":"
+
+	badUserClaims1 := mockClaims
+	badUserClaims1.ID = nonExistentUserID
+
+	type fields struct {
+		pop                  *pop.Connection
+		folderCountLimit     int
+		folderFeedCountLimit int
+	}
+	type args struct {
+		claims *helpers.AuthClaims
+	}
+	tests := []struct {
+		name            string
+		fields          fields
+		args            args
+		wantRet         []controller.Folder
+		wantErr         bool
+		wantErrContains string
+	}{
+		{
+			name: "repo nil",
+			fields: fields{
+				pop: nil,
+			},
+			args: args{
+				claims: &mockClaims,
+			},
+			wantErr:         true,
+			wantErrContains: "invalid repository",
+		},
+		{
+			name: "claims nil",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims: nil,
+			},
+			wantErr:         true,
+			wantErrContains: "invalid claims",
+		},
+		{
+			name: "invalid claims 1",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims: &invalidClaims1,
+			},
+			wantErr:         true,
+			wantErrContains: "invalid claims",
+		},
+		{
+			name: "invalid claims 2",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims: &invalidClaims2,
+			},
+			wantErr:         true,
+			wantErrContains: "invalid claims",
+		},
+		{
+			name: "bad user claims 1",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims: &badUserClaims1,
+			},
+			wantErr:         true,
+			wantErrContains: "user doesn't exist",
+		},
+		{
+			name: "folders empty",
+			fields: fields{
+				pop:                  testPop,
+				folderCountLimit:     5,
+				folderFeedCountLimit: 5,
+			},
+			args: args{
+				claims: &mockClaims,
+			},
+			wantErr: false,
+			wantRet: []controller.Folder{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := &APIPopRepository{
+				pop:                  tt.fields.pop,
+				folderCountLimit:     tt.fields.folderCountLimit,
+				folderFeedCountLimit: tt.fields.folderFeedCountLimit,
+			}
+			gotRet, err := r.Folders(tt.args.claims)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("APIPopRepository.Folders() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			} else if tt.wantErr && tt.wantErrContains != "" {
+				if !strings.Contains(err.Error(), tt.wantErrContains) {
+					t.Errorf("ChangeFolders() error = %v, wantErr %v doesn't contain %s", err, tt.wantErr, tt.wantErrContains)
+				}
+			}
+			if !reflect.DeepEqual(gotRet, tt.wantRet) {
+				t.Errorf("APIPopRepository.Folders() = %v, want %v", gotRet, tt.wantRet)
+			}
+		})
+	}
+}
+
+func TestAPIUser_ChangeFolders(t *testing.T) {
+	defer copyist.Open(t).Close()
+	beforeEachTest(t)
+
+	testPop, err := pop.Connect("test")
+	assert.NoError(t, err)
+
+	// good claims
+	mockClaims := helpers.AuthClaims{
+		ID:         testUserID,
+		Origin:     "somewhere",
+		Name:       "someone",
+		OriginName: "somewhere:someone",
+	}
+
+	// some bad claims
+	invalidClaims1 := mockClaims
+	invalidClaims1.Name = ""
+
+	invalidClaims2 := mockClaims
+	invalidClaims2.Origin = ""
+	invalidClaims2.Name = ""
+	invalidClaims2.OriginName = ":"
+
+	badUserClaims1 := mockClaims
+	badUserClaims1.ID = nonExistentUserID
+
+	type fields struct {
+		pop                  *pop.Connection
+		folderCountLimit     int
+		folderFeedCountLimit int
+	}
+	type args struct {
+		claims  *helpers.AuthClaims
+		folders []controller.Folder
+	}
+	tests := []struct {
+		name            string
+		fields          fields
+		args            args
+		wantErr         bool
+		wantErrContains string
+	}{
+		{
+			name: "repo nil",
+			fields: fields{
+				pop: nil,
+			},
+			args: args{
+				claims:  &mockClaims,
+				folders: []controller.Folder{},
+			},
+			wantErr:         true,
+			wantErrContains: "invalid repository",
+		},
+		{
+			name: "folders nil",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims:  &mockClaims,
+				folders: nil,
+			},
+			wantErr:         true,
+			wantErrContains: "invalid folders",
+		},
+		{
+			name: "claims nil",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims:  nil,
+				folders: []controller.Folder{},
+			},
+			wantErr:         true,
+			wantErrContains: "invalid claims",
+		},
+		{
+			name: "invalid claims 1",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims:  &invalidClaims1,
+				folders: []controller.Folder{},
+			},
+			wantErr:         true,
+			wantErrContains: "invalid claims",
+		},
+		{
+			name: "invalid claims 2",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims:  &invalidClaims2,
+				folders: []controller.Folder{},
+			},
+			wantErr:         true,
+			wantErrContains: "invalid claims",
+		},
+		{
+			name: "bad user claims 1",
+			fields: fields{
+				pop: testPop,
+			},
+			args: args{
+				claims:  &badUserClaims1,
+				folders: []controller.Folder{},
+			},
+			wantErr:         true,
+			wantErrContains: "user doesn't exist",
+		},
+		{
+			name: "folders empty",
+			fields: fields{
+				pop:                  testPop,
+				folderCountLimit:     5,
+				folderFeedCountLimit: 5,
+			},
+			args: args{
+				claims:  &mockClaims,
+				folders: []controller.Folder{},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &APIPopRepository{
+				pop: tt.fields.pop,
+			}
+			if err := a.ChangeFolders(tt.args.claims, tt.args.folders); (err != nil) != tt.wantErr {
+				t.Errorf("ChangeFolders() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			} else if tt.wantErr && tt.wantErrContains != "" {
+				if !strings.Contains(err.Error(), tt.wantErrContains) {
+					t.Errorf("ChangeFolders() error = %v, wantErr %v doesn't contain %s", err, tt.wantErr, tt.wantErrContains)
+				}
+			}
+		})
+	}
+}
