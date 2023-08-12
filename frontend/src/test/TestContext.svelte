39eb68b069a463a1277e0775703caac836bb6eed
spezifisch <spezifisch23@proton.me>
2023-08-12 10:20:38 +0200

    fix typing, we're not at svelte4 yet

--- frontend/src/test/TestContext.svelte
+++ frontend/src/test/TestContext.svelte
@@ -2,8 +2,8 @@
     // based on: https://svelte-recipes.netlify.app/testing/
     import { setContext, SvelteComponent } from "svelte"
 
-    export let Component: typeof SvelteComponent<any>
-    export let context_key: string
+    export let Component: typeof SvelteComponent
+    export let context_key: string | symbol
     export let context_value: any
 
     setContext(context_key, context_value)
